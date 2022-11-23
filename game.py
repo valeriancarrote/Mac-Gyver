320fe721a04fd7abb67f062d5f37d7ec84235795
Valérian Gombault <gg.valrian.gombault@geekschool.fr>
2022-11-23 19:28:29 +0100

    message

--- game.py
+++ game.py
@@ -15,8 +15,10 @@ pygame.display.set_icon(programIcon)
 bg = pygame.image.load("image/python.png")
 bg = pygame.transform.scale(bg,(800, 400))
 
+player = pygame.image.load("image/Steve.gif").convert()
 
 #rafraichissement
+win.blit(player, (0, 0))
 win.blit(bg, (0, 0))
 state = True 
 while state: 
